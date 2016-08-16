d2fafeb686fdcb03a494a436a2db2593f290ccbe
jarad <niemi@iastate.edu>
2016-08-16 17:19:50 -0500

    Add remaining data

---
+++ data/extract.R
@@ -0,0 +1,18 @@
+# Run from the data/ directory
+
+library(dplyr)
+
+read_dir = function(path, pattern) {
+  files = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  plyr::ldply(files, readr::read_csv,
+              col_types = c("Ddddiic"))
+}
+
+# Eventually need to deal with warning messages
+ToyotaSiennaGasMileage = read_dir("../data-raw", "*.csv") %>%
+  dplyr::select_("date", "fuel", "cost", "miles", "ethanol", "octane")
+
+save(ToyotaSiennaGasMileage, file="ToyotaSiennaGasMileage.RData")

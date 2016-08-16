e66d8a5c729c3bf050d595a53e287be89edd6bdb
jarad <niemi@iastate.edu>
2016-08-16 15:23:34 -0500

    Add R file for data documentation

---
+++ R/ToyotaSiennaGasMileage.R
@@ -0,0 +1,14 @@
+#' Gas consumption data for a 2011 Toyota Sienna
+#' 
+#' A dataset containing gas consumption data for a 2011 Toyota Sienna. 
+#' 
+#' @format A data frame with some rows and 6 variables:
+#' \describe{
+#'   \item{date}{date}
+#'   \item{fuel}{amount of fuel, in gallons}
+#'   \item{miles}{miles driven since last refueling}
+#'   \item{cost}{cost of fuel, in US dollars}
+#'   \item{ethanol}{amount of ethanol, percentage}
+#'   \item{octane}{octane rating}
+#' }
+"ToyotaSiennaGasMileage"

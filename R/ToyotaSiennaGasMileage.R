e9d7e794e3094b6ec6f9a02345e5ead9388adae1
jarad <niemi@iastate.edu>
2016-08-17 06:24:43 -0500

    Update documentation

--- R/ToyotaSiennaGasMileage.R
+++ R/ToyotaSiennaGasMileage.R
@@ -1,8 +1,10 @@
 #' Gas consumption data for a 2011 Toyota Sienna
 #' 
-#' A dataset containing gas consumption data for a 2011 Toyota Sienna. 
+#' Each time I refueled my 2011 Toyota Sienna, I recorded the \code{date},
+#' amount of \code{fuel}, \code{miles} driven since last refueling, \code{cost}
+#' of the gasoline, the \code{ethanol} percentage, and the \code{octane}.  
 #' 
-#' @format A data frame with some rows and 6 variables:
+#' @format A data frame with 6 variables:
 #' \describe{
 #'   \item{date}{date}
 #'   \item{fuel}{amount of fuel, in gallons}

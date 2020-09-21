f24e0db26780036af5e7500224e232b6301ca803
jarad <niemi@iastate.edu>
2020-09-21 10:16:47 -0500

    Add 20200211 data
    first google sheets file

--- data-raw/ToyotaSiennaGasMileage.R
+++ data-raw/ToyotaSiennaGasMileage.R
@@ -2,10 +2,51 @@ library("dplyr")
 library("tidyr")
 library("readr")
 
-my_read_csv = function(f, into) {
-  readr::read_csv(f) %>%
-    mutate(file=f) %>%
-    separate(file, into) 
+liters_per_gallon = 3.78541
+USD_per_CAD = 1.32
+
+read_old_csv = function(f, into) {
+  print(paste("Reading",f,"\n"))
+  
+  d = readr::read_csv(f) %>%
+    dplyr::mutate(file = f,
+                  station = notes) %>%
+    tidyr::separate(file, into) 
+  
+  if (!("liters" %in% names(d))) 
+    return(d)
+  
+  if (!("USD" %in% names(d)))
+    d$USD <- NA
+  
+  d %>%
+    dplyr::mutate(
+      inCanada = ifelse(is.na(USD), TRUE, FALSE),
+      gallons  = ifelse(inCanada, liters / liters_per_gallon, gallons),
+      USD      = ifelse(inCanada, CAD * USD_per_CAD,          USD    ))
+}
+
+read_google_csv = function(f, into) {
+  print(paste("Reading",f,"\n"))
+  
+  d = readr::read_csv(f) %>%
+    dplyr::mutate(file = f) %>%
+    tidyr::separate(file, into) %>%
+    dplyr::mutate(
+      date = sub(" .*", "", Timestamp),
+      date = as.Date(date, format = "%m/%d/%Y"),
+      
+      USD  = Cost * (Currency == "USD") + Cost * USD_per_CAD * (Currency == "CAD"),
+      gallons = `Amount of fuel` * (Units == "Gallons") +
+        `Amount of fuel` / liters_per_gallon * (Units == "Liters")
+    ) %>%
+    dplyr::rename(
+      mileage = Mileage,
+      octane  = Octane,
+      ethanol = `Ethanol %`,
+      station = Station,
+      notes   = Notes
+    )
 }
 
 read_dir = function(path, pattern, into) {
@@ -13,18 +54,24 @@ read_dir = function(path, pattern, into) {
                      pattern = pattern,
                      recursive = TRUE,
                      full.names = TRUE)
-  plyr::ldply(files, my_read_csv, into = into)
+  
+  dates = as.Date(substr(files, 9, 16), format = "%Y%m%d")
+  old_files = files[dates < as.Date("2020-01-01")]
+  google_files = files[dates > as.Date("2020-01-01")]
+  
+  bind_rows(
+    plyr::ldply(old_files,    read_old_csv,    into = into),
+    plyr::ldply(google_files, read_google_csv, into = into),
+  )
 }
 
 ToyotaSiennaGasMileage <- read_dir(path = "mileage", 
                                    pattern = "*.csv",
                                    into = c("dir","ymd","extension")) %>%
-  mutate(
-    inCanada = ifelse(is.na(USD), TRUE, FALSE),
-    gallons  = ifelse(inCanada, liters / 3.78541, gallons),
-    USD      = ifelse(inCanada, CAD * 1.32,       USD    )) %>%
+  dplyr::mutate(
+    ) %>%
   
-  select(date, gallons, USD, miles, ethanol, octane, notes, inCanada) %>%
+  select(date, gallons, USD, miles, ethanol, octane, station, notes, inCanada) %>%
   
   mutate(date = as.Date(date),
          
